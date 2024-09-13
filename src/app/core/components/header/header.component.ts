eaaed2c885aaeecf6a7827612822b85e392bed29
Christian Lisangola <jochri3@gmail.com>
2024-09-13 23:24:16 +0300

    Commit 27.1:(Avec Observables)Exercice “version” sur les services & observables

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { VersionService } from '../../services/version.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  currentVersion!: number;
 
+  constructor(private versionService: VersionService) {}
+
+  ngOnInit() {
+    this.versionService.numVersion$.subscribe((value) => {
+      this.currentVersion = value;
+    });
+  }
 }
