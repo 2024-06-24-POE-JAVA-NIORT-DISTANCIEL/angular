920f55ac94e793958a0afbdcf3de6c67ea0fb30a
Christian Lisangola <jochri3@gmail.com>
2024-09-17 17:23:39 +0300

    35:ReactiveFormsModule

--- src/app/orders/components/form-order/form-order.component.ts
+++ src/app/orders/components/form-order/form-order.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { StatusOrder } from '../../../core/enums/status-order.enum';
 import { Order } from '../../../core/models/order';
 
@@ -10,4 +11,25 @@ import { Order } from '../../../core/models/order';
 export class FormOrderComponent {
   status = Object.values(StatusOrder);
   @Input() init!: Order;
+  form!: FormGroup;
+
+  // form2 = new FormGroup({
+  //   tjmHt: new FormControl(this.init.tjmHt),
+  //   nbJours: new FormControl(this.init.nbJours),
+  // });
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      tjmHt: [this.init.tjmHt],
+      nbJours: [this.init.nbJours],
+      tva: [this.init.tva],
+      state: [this.init.state],
+      client: [this.init.client],
+      comment: [this.init.comment],
+      typePresta: [this.init.typePresta],
+      id: [this.init.id],
+    });
+  }
 }
