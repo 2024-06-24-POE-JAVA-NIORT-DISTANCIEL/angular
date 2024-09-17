ae77d3290027edbf315ec9e594b1712d6a608607
Christian Lisangola <jochri3@gmail.com>
2024-09-17 15:51:23 +0300

    32.1.:(Best practice)StateDirective & HostBinding

--- src/app/shared/directives/status.directive.ts
+++ src/app/shared/directives/status.directive.ts
@@ -1,9 +1,16 @@
-import { Directive, Input } from '@angular/core';
+import { Directive, HostBinding, Input } from '@angular/core';
 
 @Directive({
-  selector: '[appStatus]',
+  selector: '[appState]',
 })
 export class StatusDirective {
   @Input() appState!: string;
+  @HostBinding('class') tdClassName!: string;
   constructor() {}
+
+  ngOnChanges() {
+    console.log(this.appState);
+    this.tdClassName = `state-${this.appState.toLocaleLowerCase()}`;
+    console.log(this.tdClassName);
+  }
 }
