d7f827e8a9694663f5a949c016fe62d384f23af4
Christian Lisangola <jochri3@gmail.com>
2024-09-17 16:23:09 +0300

    33:Btn component ready to (re)use

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { IconsModule } from '../icons/icons.module';
 import { TemplatesModule } from '../templates/templates.module';
 import { BtnComponent } from './components/btn/btn.component';
 import { TableLightComponent } from './components/table-light/table-light.component';
-import { TotalPipe } from './pipes/total.pipe';
 import { StatusDirective } from './directives/status.directive';
+import { TotalPipe } from './pipes/total.pipe';
 
 @NgModule({
   declarations: [BtnComponent, TableLightComponent, TotalPipe, StatusDirective],
-  imports: [CommonModule],
-  exports: [TemplatesModule, IconsModule, BtnComponent, TableLightComponent, TotalPipe, StatusDirective],
+  imports: [CommonModule, RouterModule],
+  exports: [
+    TemplatesModule,
+    IconsModule,
+    BtnComponent,
+    TableLightComponent,
+    TotalPipe,
+    StatusDirective,
+  ],
 })
 export class SharedModule {}
